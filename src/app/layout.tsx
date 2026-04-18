67f191c99a0653bee26e40da7aee2991c59ee620
kientndev <kien.eat.pizza@gmail.com>
2026-04-18 18:16:22 +0700

    committed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import { AppProvider } from "../context/AppContext";
 import Navbar from "../components/Navbar";
 import { Toaster } from "sonner";
@@ -33,13 +34,15 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col bg-[#0b0e14] text-[#fafafa]">
-        <ConvexClientProvider>
-          <AppProvider>
-            <Toaster position="bottom-right" richColors theme="dark" />
-            <Navbar />
-            {children}
-          </AppProvider>
-        </ConvexClientProvider>
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <AppProvider>
+              <Toaster position="bottom-right" richColors theme="dark" />
+              <Navbar />
+              {children}
+            </AppProvider>
+          </ConvexClientProvider>
+        </ClerkProvider>
         <GoogleAnalytics gaId="G-WR6V55XDBM" />
       </body>
     </html>

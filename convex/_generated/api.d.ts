766db9c89840a1c0532d4b091051f1a9a43f734a
kientndev <kien.eat.pizza@gmail.com>
2026-05-11 21:25:01 +0700

    commti

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -9,6 +9,7 @@
  */
 
 import type * as users from "../users.js";
+import type * as partners from "../partners.js";
 
 import type {
   ApiFromModules,
@@ -18,6 +19,7 @@ import type {
 
 declare const fullApi: ApiFromModules<{
   users: typeof users;
+  partners: typeof partners;
 }>;
 
 /**
